d22494231c51384d4f173af579cd2288e46f861c
daf <daf@mail.com>
2023-06-14 15:27:41 +0200

    MainActivity.kt transformed appViewModel instance to compose instantiation viewModel<>()

--- app/src/main/java/com/deslomator/tagtimer/MainActivity.kt
+++ app/src/main/java/com/deslomator/tagtimer/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.deslomator.tagtimer
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Room
 import com.deslomator.tagtimer.dao.SessionsDatabase
 import com.deslomator.tagtimer.ui.SessionsScreen
@@ -20,39 +21,41 @@ import com.deslomator.tagtimer.viewmodel.AppViewModel
 
 class MainActivity : ComponentActivity() {
 
-    private val db by lazy {
-        Room.databaseBuilder(
-            applicationContext,
-            SessionsDatabase::class.java,
-            "sessions.db"
-        ).build()
-    }
-
-    private val viewModel by viewModels<AppViewModel>(
-        factoryProducer = {
-            object : ViewModelProvider.Factory {
-                override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                    return AppViewModel(db.appDao) as T
-                }
-            }
-        }
-    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TagTimerTheme {
+                val appViewModel = viewModel<AppViewModel>(
+                    factory = object : ViewModelProvider.Factory {
+                        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                            Log.d(TAG, "factory, creating viewModel")
+                            val db by lazy {
+                                Room.databaseBuilder(
+                                    applicationContext,
+                                    SessionsDatabase::class.java,
+                                    "sessions.db"
+                                ).build()
+                            }
+                            return AppViewModel(db.appDao) as T
+                        }
+                    }
+                )
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val uiState by viewModel.state.collectAsState()
+                    val uiState by appViewModel.state.collectAsState()
                     SessionsScreen(
                         uiState,
-                        viewModel::onAction
+                        appViewModel::onAction
                     )
                 }
             }
         }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
