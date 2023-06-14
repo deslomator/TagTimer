49767e60351770ce5187d39ca0304f49fdb9d190
daf <daf@mail.com>
2023-06-14 02:46:24 +0200

    ColorPicker.kt persist grid state across orientation changes

--- app/src/main/java/com/deslomator/tagtimer/ui/ColorPicker.kt
+++ app/src/main/java/com/deslomator/tagtimer/ui/ColorPicker.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,7 +33,7 @@ fun ColorPicker(
     selectedColor: Color,
     onItemClick: (Int) -> Unit,
 ) {
-    var showGrid by remember { mutableStateOf(false) }
+    var showGrid by rememberSaveable { mutableStateOf(false) }
 
     if (!showGrid) {
         Box(
