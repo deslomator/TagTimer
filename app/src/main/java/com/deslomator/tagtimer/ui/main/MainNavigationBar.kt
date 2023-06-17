64fc74555651807aa10396c4f5145d4a757dc4cd
daf <daf@mail.com>
2023-06-17 19:05:35 +0200

    Extracted MainNavigationBar

---
+++ app/src/main/java/com/deslomator/tagtimer/ui/main/MainNavigationBar.kt
@@ -0,0 +1,56 @@
+package com.deslomator.tagtimer.ui.main
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+
+@Composable
+fun MainNavigationBar(
+    backStackEntry: State<NavBackStackEntry?>,
+    innerNavHostController: NavHostController
+) {
+    NavigationBar {
+        val tabItems = listOf(
+            BottomNavigationScreen.Sessions,
+//        BottomNavigationScreens.Tags,
+            BottomNavigationScreen.Trash,
+        )
+        tabItems.forEach { tab ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(tab.icon),
+                        contentDescription = tab.route
+                    )
+                },
+                label = {
+                    Text(stringResource(id = tab.stringId))
+                },
+                selected = tab.route == backStackEntry.value?.destination?.route,
+                onClick = {
+                    innerNavHostController.navigate(tab.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(innerNavHostController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
