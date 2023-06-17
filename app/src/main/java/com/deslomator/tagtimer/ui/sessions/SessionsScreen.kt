db328125a6bbfdda97b93fb0a5a57624272224f4
daf <daf@mail.com>
2023-06-17 09:54:11 +0200

    Some cleanup of snackbar code in SessionsScreen.kt and SessionsTrashScreen.kt

--- app/src/main/java/com/deslomator/tagtimer/ui/sessions/SessionsScreen.kt
+++ app/src/main/java/com/deslomator/tagtimer/ui/sessions/SessionsScreen.kt
@@ -1,6 +1,5 @@
 package com.deslomator.tagtimer.ui.sessions
 
-import android.util.Log
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
@@ -27,15 +26,10 @@ fun SessionsScreen(
     if (state.showSnackbar) {
         LaunchedEffect(key1 = snackbarHostState) {
 //            Log.d(TAG, "launching snackbar")
-            val result = snackbarHostState.showSnackbar(
+            snackbarHostState.showSnackbar(
                 message = context.getString(R.string.session_sent_to_trash),
                 duration = SnackbarDuration.Short
             )
-            when (result) {
-                SnackbarResult.Dismissed -> {}
-                SnackbarResult.ActionPerformed -> {}
-            }
-//            Log.d(TAG, "hiding snackbar")
             onAction(SessionsScreenAction.HideSnackbar)
         }
     }
--- app/src/main/java/com/deslomator/tagtimer/ui/sessionsTrash/SessionsTrashScreen.kt
+++ app/src/main/java/com/deslomator/tagtimer/ui/sessionsTrash/SessionsTrashScreen.kt
@@ -1,16 +1,13 @@
 package com.deslomator.tagtimer.ui.sessionsTrash
 
-import android.util.Log
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import com.deslomator.tagtimer.R
 import com.deslomator.tagtimer.action.AppAction
 import com.deslomator.tagtimer.action.SessionsTrashAction
 import com.deslomator.tagtimer.state.SessionsTrashState
@@ -27,26 +24,22 @@ fun SessionsTrashScreen(
     if (state.showSnackBar) {
         LaunchedEffect(key1 = snackbarHostState) {
 //            Log.d(TAG, "launching trash snackbar")
-            val result = snackbarHostState.showSnackbar(
+            snackbarHostState.showSnackbar(
                 message = context.getString(state.snackbarMessage),
                 duration = SnackbarDuration.Short
             )
-//            Log.d(TAG, "trash snackbar waiting result")
-            /*when (result) {
-                SnackbarResult.Dismissed -> {}
-                SnackbarResult.ActionPerformed -> {}
-            }*/
-//            Log.d(TAG, "Hiding trash snackbar")
             onAction(SessionsTrashAction.HideSnackbar)
         }
     }
     Scaffold(
-        topBar = { SessionsTrashTopBar(
-            onBackClick = {
-                onAction(SessionsTrashAction.HideSnackbar)
-                onAppAction(AppAction.activateScreen(Screen.SESSIONS))
-            },
-        ) },
+        topBar = {
+            SessionsTrashTopBar(
+                onBackClick = {
+                    onAction(SessionsTrashAction.HideSnackbar)
+                    onAppAction(AppAction.activateScreen(Screen.SESSIONS))
+                },
+            )
+        },
         content = { paddingValues ->
             SessionsTrashContent(
                 paddingValues = paddingValues,
