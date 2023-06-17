ab9bbaf53bb28ab814d9845f9a8c9a56c9d15e70
daf <daf@mail.com>
2023-06-17 19:06:47 +0200

    SessionsTopBar pass back stack state to customize actions

--- app/src/main/java/com/deslomator/tagtimer/ui/sessions/SessionsTopBar.kt
+++ app/src/main/java/com/deslomator/tagtimer/ui/sessions/SessionsTopBar.kt
@@ -6,13 +6,16 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavBackStackEntry
 import com.deslomator.tagtimer.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SessionsTopBar(
+    backStackEntry: State<NavBackStackEntry?>,
     onNewSessionClick: () -> Unit,
 ) {
     TopAppBar(
