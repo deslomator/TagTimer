b871c5aa4d02e7330d8263c79301aa1a314e5eff
daf <daf@mail.com>
2023-06-17 19:06:14 +0200

    MainScreen added trash snackbar

--- app/src/main/java/com/deslomator/tagtimer/ui/main/MainScreen.kt
+++ app/src/main/java/com/deslomator/tagtimer/ui/main/MainScreen.kt
@@ -1,20 +1,13 @@
 package com.deslomator.tagtimer.ui.main
 
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -48,49 +41,27 @@ fun MainScreen(
             onSessionsAction(SessionsScreenAction.HideSnackbar)
         }
     }
+    if (sessionsTrashState.showSnackBar) {
+        LaunchedEffect(key1 = snackbarHostState) {
+//            Log.d(TAG, "launching snackbar")
+            snackbarHostState.showSnackbar(
+                message = context.getString(sessionsTrashState.snackbarMessage),
+                duration = SnackbarDuration.Short
+            )
+            onSessionsTrashAction(SessionsTrashAction.HideSnackbar)
+        }
+    }
     Scaffold(
         topBar = {
             SessionsTopBar(
+                backStackEntry = backStackEntry,
                 onNewSessionClick = { onSessionsAction(SessionsScreenAction.AddNewSessionClicked) },
             )
         },
         bottomBar = {
-            NavigationBar {
-                val tabItems = listOf(
-                    BottomNavigationScreen.Sessions,
-//        BottomNavigationScreens.Tags,
-                    BottomNavigationScreen.Trash,
-                )
-                tabItems.forEach { tab ->
-                    NavigationBarItem(
-                        icon = {
-                            Icon(
-                                painter = painterResource(tab.icon),
-                                contentDescription = tab.route
-                            )
-                        },
-                        label = {
-                            Text(stringResource(id = tab.stringId))
-                        },
-                        selected = tab.route == backStackEntry.value?.destination?.route,
-                        onClick = {
-                            innerNavHostController.navigate(tab.route) {
-                                // Pop up to the start destination of the graph to
-                                // avoid building up a large stack of destinations
-                                // on the back stack as users select items
-                                popUpTo(innerNavHostController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-                                // Avoid multiple copies of the same destination when
-                                // reselecting the same item
-                                launchSingleTop = true
-                                // Restore state when reselecting a previously selected item
-                                restoreState = true
-                            }
-                        }
-                    )
-                }
-            }
+            MainNavigationBar(
+                backStackEntry,
+                innerNavHostController)
         },
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         content = { paddingValues ->
@@ -106,3 +77,4 @@ fun MainScreen(
         }
     )
 }
+
