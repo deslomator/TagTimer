7a222da12825faf4cf7b318dc76d2053f4ab0b9d
daf <mail@mail.com>
2023-06-20 20:47:32 +0200

    Event now does not store tagId, instead copies all relevant fields from its associated tag

--- app/src/main/java/com/deslomator/tagtimer/model/Event.kt
+++ app/src/main/java/com/deslomator/tagtimer/model/Event.kt
@@ -1,14 +1,18 @@
 package com.deslomator.tagtimer.model
 
+import androidx.compose.ui.graphics.toArgb
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.deslomator.tagtimer.ui.theme.colorPickerColors
 
 @Entity
 data class Event(
     val sessionId: Int = 0,
-    val tagId: Int = 0,
     val timestampMillis: Long = 0,
     val note: String = "",
+    val category: String = "",
+    val label: String = "",
+    val color: Int = colorPickerColors[7].toArgb(),
     val inTrash: Boolean = false,
     @PrimaryKey(autoGenerate = true) val id: Int = 0
 )
