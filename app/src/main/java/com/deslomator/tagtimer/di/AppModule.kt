1a291d62f23bf176331ed6627a4386026b1f0c12
daf <daf@mail.com>
2023-06-22 13:21:45 +0200

    updated module build.gradle

--- app/src/main/java/com/deslomator/tagtimer/di/AppModule.kt
+++ app/src/main/java/com/deslomator/tagtimer/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.deslomator.tagtimer.di
 
 import android.content.Context
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.room.Room
 import com.deslomator.tagtimer.dao.AppDao
 import com.deslomator.tagtimer.dao.SessionsDatabase
+import com.deslomator.tagtimer.viewmodel.ActiveSessionViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,6 +26,17 @@ object AppModule {
             SessionsDatabase::class.java,
             DATABASE_NAME
         ).build().appDao
+
+    /*@Singleton
+    @Provides
+    fun provideActiveSessionViewModel(dao: AppDao) :ActiveSessionViewModel {
+
+        return viewModelFactory {
+            initializer {
+                ActiveSessionViewModel(dao)
+            }
+        }.create(ActiveSessionViewModel::class.java)
+    }*/
 }
 
 private const val DATABASE_NAME = "sessions.db"
