c0f9a0c9ca798471ecbaed1b9aae0d21be20102a
daf <mail@mail.com>
2023-07-06 16:56:44 +0200

    Imported but not used some generated theme

--- app/src/main/java/com/deslomator/tagtimer/ui/theme/Color.kt
+++ app/src/main/java/com/deslomator/tagtimer/ui/theme/Color.kt
@@ -3,6 +3,9 @@ package com.deslomator.tagtimer.ui.theme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -61,15 +64,15 @@ fun Color.brightness(): Float {
     return (0.21f * this.red) + (0.72f * this.green) + (0.07f * this.blue)
 }
 
- /*
- hue from RGB formula
+/*
+hue from RGB formula
 if (R >= G >= B) { 60 * ((G - B) / (R - B)) }
 else if (G > R >= B ) { 60 * (2 - (R - B) / (G - B)) }
 else if (G >= B > R ) { 60 * (2 + (B - R) / (G - R)) }
 else if (B > G > R  ) { 60 * (4 - (G - R) / (B - R)) }
 else if (B > R >= G ) { 60 * (4 + (R - G) / (B - G)) }
 else (R >= B > G ) { 60 * (6 - (B - G) / (R - G) }
- */
+*/
 
 fun Color.hue(): Float {
     val r = this.red
@@ -137,6 +140,175 @@ fun Colors() {
                     color = it.second.contrasted(),
                     fontSize = 13.sp
                 )
+            }
+        }
+    }
+}
+
+
+
+val md_theme_light_primary = Color(0xFF056D37)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFF9CF6B1)
+val md_theme_light_onPrimaryContainer = Color(0xFF00210C)
+val md_theme_light_secondary = Color(0xFF506352)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFD3E8D3)
+val md_theme_light_onSecondaryContainer = Color(0xFF0E1F12)
+val md_theme_light_tertiary = Color(0xFF3A656E)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFBDEAF5)
+val md_theme_light_onTertiaryContainer = Color(0xFF001F25)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFCFDF7)
+val md_theme_light_onBackground = Color(0xFF191C19)
+val md_theme_light_surface = Color(0xFFFCFDF7)
+val md_theme_light_onSurface = Color(0xFF191C19)
+val md_theme_light_surfaceVariant = Color(0xFFDDE5DA)
+val md_theme_light_onSurfaceVariant = Color(0xFF414941)
+val md_theme_light_outline = Color(0xFF717971)
+val md_theme_light_inverseOnSurface = Color(0xFFF0F1EC)
+val md_theme_light_inverseSurface = Color(0xFF2E312E)
+val md_theme_light_inversePrimary = Color(0xFF81D997)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF056D37)
+val md_theme_light_outlineVariant = Color(0xFFC1C9BF)
+val md_theme_light_scrim = Color(0xFF000000)
+
+val md_theme_dark_primary = Color(0xFF81D997)
+val md_theme_dark_onPrimary = Color(0xFF003919)
+val md_theme_dark_primaryContainer = Color(0xFF005227)
+val md_theme_dark_onPrimaryContainer = Color(0xFF9CF6B1)
+val md_theme_dark_secondary = Color(0xFFB7CCB7)
+val md_theme_dark_onSecondary = Color(0xFF233426)
+val md_theme_dark_secondaryContainer = Color(0xFF394B3B)
+val md_theme_dark_onSecondaryContainer = Color(0xFFD3E8D3)
+val md_theme_dark_tertiary = Color(0xFFA2CED9)
+val md_theme_dark_onTertiary = Color(0xFF01363F)
+val md_theme_dark_tertiaryContainer = Color(0xFF204D56)
+val md_theme_dark_onTertiaryContainer = Color(0xFFBDEAF5)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF191C19)
+val md_theme_dark_onBackground = Color(0xFFE2E3DE)
+val md_theme_dark_surface = Color(0xFF191C19)
+val md_theme_dark_onSurface = Color(0xFFE2E3DE)
+val md_theme_dark_surfaceVariant = Color(0xFF414941)
+val md_theme_dark_onSurfaceVariant = Color(0xFFC1C9BF)
+val md_theme_dark_outline = Color(0xFF8B938A)
+val md_theme_dark_inverseOnSurface = Color(0xFF191C19)
+val md_theme_dark_inverseSurface = Color(0xFFE2E3DE)
+val md_theme_dark_inversePrimary = Color(0xFF056D37)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFF81D997)
+val md_theme_dark_outlineVariant = Color(0xFF414941)
+val md_theme_dark_scrim = Color(0xFF000000)
+
+val seed = Color(0xFF076D37)
+
+
+@Preview
+@Composable
+fun Colors2() {
+    val colorsLight = listOf(
+        "md_theme_light_primary" to md_theme_light_primary,
+        "md_theme_light_onPrimary" to md_theme_light_onPrimary,
+        "md_theme_light_primaryContainer" to md_theme_light_primaryContainer,
+        "md_theme_light_onPrimaryContainer" to md_theme_light_onPrimaryContainer,
+        "md_theme_light_secondary" to md_theme_light_secondary,
+        "md_theme_light_onSecondary" to md_theme_light_onSecondary,
+        "md_theme_light_secondaryContainer" to md_theme_light_secondaryContainer,
+        "md_theme_light_onSecondaryContainer" to md_theme_light_onSecondaryContainer,
+        "md_theme_light_tertiary" to md_theme_light_tertiary,
+        "md_theme_light_onTertiary" to md_theme_light_onTertiary,
+        "md_theme_light_tertiaryContainer" to md_theme_light_tertiaryContainer,
+        "md_theme_light_onTertiaryContainer" to md_theme_light_onTertiaryContainer,
+        "md_theme_light_error" to md_theme_light_error,
+        "md_theme_light_errorContainer" to md_theme_light_errorContainer,
+        "md_theme_light_onError" to md_theme_light_onError,
+        "md_theme_light_onErrorContainer" to md_theme_light_onErrorContainer,
+        "md_theme_light_background" to md_theme_light_background,
+        "md_theme_light_onBackground" to md_theme_light_onBackground,
+        "md_theme_light_surface" to md_theme_light_surface,
+        "md_theme_light_onSurface" to md_theme_light_onSurface,
+        "md_theme_light_surfaceVariant" to md_theme_light_surfaceVariant,
+        "md_theme_light_onSurfaceVariant" to md_theme_light_onSurfaceVariant,
+        "md_theme_light_outline" to md_theme_light_outline,
+        "md_theme_light_inverseOnSurface" to md_theme_light_inverseOnSurface,
+        "md_theme_light_inverseSurface" to md_theme_light_inverseSurface,
+        "md_theme_light_inversePrimary" to md_theme_light_inversePrimary,
+        "md_theme_light_shadow" to md_theme_light_shadow,
+        "md_theme_light_surfaceTint" to md_theme_light_surfaceTint,
+        "md_theme_light_outlineVariant" to md_theme_light_outlineVariant,
+        "md_theme_light_scrim" to md_theme_light_scrim,
+    )
+    val colorsDark = listOf(
+        "md_theme_dark_primary" to md_theme_dark_primary,
+        "md_theme_dark_onPrimary" to md_theme_dark_onPrimary,
+        "md_theme_dark_primaryContainer" to md_theme_dark_primaryContainer,
+        "md_theme_dark_onPrimaryContainer" to md_theme_dark_onPrimaryContainer,
+        "md_theme_dark_secondary" to md_theme_dark_secondary,
+        "md_theme_dark_onSecondary" to md_theme_dark_onSecondary,
+        "md_theme_dark_secondaryContainer" to md_theme_dark_secondaryContainer,
+        "md_theme_dark_onSecondaryContainer" to md_theme_dark_onSecondaryContainer,
+        "md_theme_dark_tertiary" to md_theme_dark_tertiary,
+        "md_theme_dark_onTertiary" to md_theme_dark_onTertiary,
+        "md_theme_dark_tertiaryContainer" to md_theme_dark_tertiaryContainer,
+        "md_theme_dark_onTertiaryContainer" to md_theme_dark_onTertiaryContainer,
+        "md_theme_dark_error" to md_theme_dark_error,
+        "md_theme_dark_errorContainer" to md_theme_dark_errorContainer,
+        "md_theme_dark_onError" to md_theme_dark_onError,
+        "md_theme_dark_onErrorContainer" to md_theme_dark_onErrorContainer,
+        "md_theme_dark_background" to md_theme_dark_background,
+        "md_theme_dark_onBackground" to md_theme_dark_onBackground,
+        "md_theme_dark_surface" to md_theme_dark_surface,
+        "md_theme_dark_onSurface" to md_theme_dark_onSurface,
+        "md_theme_dark_surfaceVariant" to md_theme_dark_surfaceVariant,
+        "md_theme_dark_onSurfaceVariant" to md_theme_dark_onSurfaceVariant,
+        "md_theme_dark_outline" to md_theme_dark_outline,
+        "md_theme_dark_inverseOnSurface" to md_theme_dark_inverseOnSurface,
+        "md_theme_dark_inverseSurface" to md_theme_dark_inverseSurface,
+        "md_theme_dark_inversePrimary" to md_theme_dark_inversePrimary,
+        "md_theme_dark_shadow" to md_theme_dark_shadow,
+        "md_theme_dark_surfaceTint" to md_theme_dark_surfaceTint,
+        "md_theme_dark_outlineVariant" to md_theme_dark_outlineVariant,
+        "md_theme_dark_scrim" to md_theme_dark_scrim,
+    )
+    Column {
+        colorsLight.forEach {
+            Box(
+                modifier = Modifier
+                    .background(it.second)
+                    .width(150.dp)
+                    .padding(3.dp)
+            ) {
+                Text(
+                    text = it.first,
+                    color = it.second.contrasted(),
+                    fontSize = 8.sp
+                )
+                Spacer(modifier = Modifier.height(3.dp))
+            }
+        }
+        Spacer(modifier = Modifier.height(30.dp))
+        colorsDark.forEach {
+            Box(
+                modifier = Modifier
+                    .background(it.second)
+                    .width(150.dp)
+                    .padding(3.dp)
+            ) {
+                Text(
+                    text = it.first,
+                    color = it.second.contrasted(),
+                    fontSize = 8.sp
+                )
+                Spacer(modifier = Modifier.height(3.dp))
             }
         }
     }
