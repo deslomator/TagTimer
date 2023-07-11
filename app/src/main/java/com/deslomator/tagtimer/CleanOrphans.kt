82c9bb5db0fb5a346b833f8f7a9292582e0a239f
daf <daf@mail.com>
2023-07-11 06:44:21 +0200

    cleanOrphans async calls to AppDao

--- app/src/main/java/com/deslomator/tagtimer/CleanOrphans.kt
+++ app/src/main/java/com/deslomator/tagtimer/CleanOrphans.kt
@@ -2,18 +2,23 @@ package com.deslomator.tagtimer
 
 import android.util.Log
 import com.deslomator.tagtimer.dao.AppDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 suspend fun cleanOrphans(dao: AppDao) {
-    delay(4000)
-    val oe = dao.clearOrphanEvents()
-    val out = dao.clearOrphanPreSelectedPersons()
-    val out2 = dao.clearOrphanPreSelectedPlaces()
-    val out3 = dao.clearOrphanPreSelectedTags()
-    Log.d(TAG, "dao.clearOrphanEvents(): $oe")
-    Log.d(TAG, "dao.clearOrphanPreSelectedPersons(): $out")
-    Log.d(TAG, "dao.clearOrphanPreSelectedPlaces(): $out2")
-    Log.d(TAG, "dao.clearOrphanPreSelectedTags(): $out3")
+    withContext(Dispatchers.IO) {
+        delay(4000)
+        val oe = async { dao.clearOrphanEvents() }
+        val out = async { dao.clearOrphanPreSelectedPersons() }
+        val out2 = async { dao.clearOrphanPreSelectedPlaces() }
+        val out3 = async { dao.clearOrphanPreSelectedTags() }
+        Log.d(TAG, "dao.clearOrphanEvents(): ${oe.await()}")
+        Log.d(TAG, "dao.clearOrphanPreSelectedPersons(): ${out.await()}")
+        Log.d(TAG, "dao.clearOrphanPreSelectedPlaces(): ${out2.await()}")
+        Log.d(TAG, "dao.clearOrphanPreSelectedTags(): ${out3.await()}")
+    }
 }
 
 private const val TAG = "cleanOrphans"
