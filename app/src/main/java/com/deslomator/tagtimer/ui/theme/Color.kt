a2cc0969dac644a8a7a8ff18b0b5a050291c8166
daf <daf@mail.com>
2023-07-17 14:01:06 +0200

    Adding colorPickerColors and updating TagTimerTheme

--- app/src/main/java/com/deslomator/tagtimer/ui/theme/Color.kt
+++ app/src/main/java/com/deslomator/tagtimer/ui/theme/Color.kt
@@ -32,34 +32,44 @@ val OnDarkBackground = Color(0xFFF3ECFF)
 val OnLightBackground = Color(0xFF26242C)
 
 val colorPickerColors: List<Color> = listOf(
-    Color(0xffd21212),
-    Color(0xff0aa00a),
-    Color(0xff368cf4),
-    Color(0xffff8707),
-    Color(0xffff43b3),
-    Color(0xffacb303),
-    Color(0xffaa4400),
-    Color(0xff892ca0),
-    Color(0xff888888),
-    Color(0xff000000),
-    Color(0xff003366),
     Color(0xffffffff),
-    Color(0xFFCB2F54),
-    Color(0xFF95122E),
-    Color(0xFF26637E),
-    Color(0xff7067cf),
-    Color(0xff001582),
-    Color(0xff04a48f),
+    Color(0xfffff8f0),
+    Color(0xffffebeb),
+    Color(0xFFfffd9e),
     Color(0xFFEBEE3C),
-    Color(0xff582417),
-    Color(0xff005016),
-    Color(0xff870058),
-    Color(0xff272727),
-    Color(0xff5ccb5f),
+    Color(0xffacb303),
+    Color(0xffF6AE2D),
     Color(0xffD39F26),
+    Color(0xffffd29e),
+    Color(0xffff8707),
+    Color(0xffaa4400),
     Color(0xffFE654F),
+    Color(0xfff5e6d5),
+    Color(0xfff9bcde),
+    Color(0xffff43b3),
+    Color(0xffd4a1fa),
+    Color(0xFF95122E),
+    Color(0xffd21212),
+    Color(0xFFCB2F54),
+    Color(0xff870058),
+    Color(0xff7067cf),
+    Color(0xff892ca0),
+    Color(0xff582417),
+    Color(0xffa5f9c2),
+    Color(0xff0aa00a),
+    Color(0xff5ccb5f),
+    Color(0xff04a48f),
+    Color(0xff005016),
+    Color(0xFF26637E),
+    Color(0xff9fdcf9),
+    Color(0xff87cefa),
+    Color(0xff368cf4),
     Color(0xff334AFF),
-    Color(0xffF6AE2D)
+    Color(0xff003366),
+    Color(0xff001582),
+    Color(0xff888888),
+    Color(0xff272727),
+    Color(0xff000000),
 )
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -205,7 +215,7 @@ val md_theme_light_error = Color(0xFFBA1A1A)
 val md_theme_light_errorContainer = Color(0xFFFFDAD6)
 val md_theme_light_onError = Color(0xFFFFFFFF)
 val md_theme_light_onErrorContainer = Color(0xFF410002)
-val md_theme_light_background = Color(0xFFFAFDEE)
+val md_theme_light_background = Color(0xFFC8E067)
 val md_theme_light_onBackground = Color(0xFF191C19)
 val md_theme_light_surface = Color(0xFFFCFDF7)
 val md_theme_light_onSurface = Color(0xFF191C19)
