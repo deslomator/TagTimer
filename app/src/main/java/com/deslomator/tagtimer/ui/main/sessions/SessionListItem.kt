1958952264b17987c74f5803eef765ca49ce9557
daf <daf@mail.com>
2025-03-08 15:23:05 +0100

    Added archived and trashed badge to SessionListItem

---
+++ app/src/main/java/com/deslomator/tagtimer/ui/main/sessions/SessionListItem.kt
@@ -0,0 +1,131 @@
+package com.deslomator.tagtimer.ui.main.sessions
+
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.deslomator.tagtimer.R
+import com.deslomator.tagtimer.action.SessionsTabAction
+import com.deslomator.tagtimer.model.Session
+import com.deslomator.tagtimer.model.type.ItemState
+import com.deslomator.tagtimer.navigation.screen.ActiveSessionScreen
+import com.deslomator.tagtimer.ui.MyListItem
+import com.deslomator.tagtimer.ui.theme.contrasted
+import com.deslomator.tagtimer.util.toColor
+import com.deslomator.tagtimer.util.toDateTime
+
+@Composable
+fun SessionListItem(
+    session: Session,
+    outerNavHostController: NavHostController,
+    onAction: (SessionsTabAction) -> Unit
+) {
+    Box(
+        modifier = Modifier.wrapContentSize(),
+        contentAlignment = Alignment.TopEnd
+    ) {
+        MyListItem(
+            colors = CardDefaults.cardColors(
+                contentColor = session.color.toColor().contrasted(),
+                containerColor = session.color.toColor(),
+            ),
+            shape = RoundedCornerShape(25.dp),
+            border = BorderStroke(1.dp, Color.LightGray),
+            item = session,
+            leadingIcon = null,
+            onLeadingClick = { },
+            onItemClick = {
+                outerNavHostController.navigate(
+                    ActiveSessionScreen(sessionId = session.id!!)
+                )
+            },
+            onLongClick = {
+                onAction(SessionsTabAction.EditSessionClicked(session))
+            },
+        ) { item ->
+            Spacer(modifier = Modifier.width(10.dp))
+            Column(
+                modifier = Modifier.weight(1F)
+            ) {
+                Text(item.name)
+                Text(item.sessionDateMillis.toDateTime())
+            }
+            Column(
+                modifier = Modifier.padding(end = 5.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(stringResource(R.string.events_count, item.eventCount))
+                if (item.running) {
+                    val transition = rememberInfiniteTransition(label = "running_icon")
+                    val alphaCycle by transition.animateFloat(
+                        initialValue = .3F,
+                        targetValue = 1F,
+                        animationSpec = InfiniteRepeatableSpec(
+                            animation = tween(durationMillis = 1000),
+                            repeatMode = RepeatMode.Reverse
+                        ),
+                        label = "alpha_cycle"
+                    )
+                    Icon(
+                        modifier = Modifier.graphicsLayer { alpha = alphaCycle },
+                        painter = painterResource(R.drawable.running),
+                        contentDescription = "running",
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+        }
+
+        when (session.state) {
+            ItemState.ENABLED -> {}
+            ItemState.ARCHIVED -> {
+                Icon(
+                modifier = Modifier.offset(x = 5.dp, y = -10.dp)
+                    .size(40.dp)
+                    .alpha(.6F),
+                    painter = painterResource(R.drawable.folder),
+                    contentDescription = "running",
+                )
+            }
+
+            ItemState.TRASHED -> {
+                Icon(
+                    modifier = Modifier.offset(x = 5.dp, y = -10.dp)
+                        .size(40.dp)
+                        .alpha(.6F),
+                    painter = painterResource(R.drawable.trash),
+                    contentDescription = "running",
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/deslomator/tagtimer/ui/main/sessions/SessionsScreenContent.kt
+++ app/src/main/java/com/deslomator/tagtimer/ui/main/sessions/SessionsScreenContent.kt
@@ -1,47 +1,22 @@
 package com.deslomator.tagtimer.ui.main.sessions
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.core.InfiniteRepeatableSpec
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.deslomator.tagtimer.R
 import com.deslomator.tagtimer.action.SessionsTabAction
 import com.deslomator.tagtimer.model.type.DialogState
-import com.deslomator.tagtimer.navigation.screen.ActiveSessionScreen
 import com.deslomator.tagtimer.state.SessionsScreenState
-import com.deslomator.tagtimer.ui.MyListItem
-import com.deslomator.tagtimer.ui.theme.contrasted
-import com.deslomator.tagtimer.util.toColor
-import com.deslomator.tagtimer.util.toDateTime
 
 @Composable
 fun SessionsScreenContent(
@@ -69,57 +44,7 @@ fun SessionsScreenContent(
                 items = state.sessions,
                 key = { it.id!! }
             ) { session ->
-                MyListItem(
-                    colors = CardDefaults.cardColors(
-                        contentColor = session.color.toColor().contrasted(),
-                        containerColor = session.color.toColor(),
-                    ),
-                    shape = RoundedCornerShape(25.dp),
-                    border = BorderStroke(1.dp, Color.LightGray),
-                    item = session,
-                    leadingIcon = null,
-                    onLeadingClick = { },
-                    onItemClick = {
-                        outerNavHostController.navigate(
-                            ActiveSessionScreen(sessionId = session.id!!)
-                        )
-                    },
-                    onLongClick = {
-                        onAction(SessionsTabAction.EditSessionClicked(session))
-                    },
-                ) { item ->
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Column(
-                        modifier = Modifier.weight(1F)
-                    ) {
-                        Text(item.name)
-                        Text(item.sessionDateMillis.toDateTime())
-                    }
-                    Column(
-                        modifier = Modifier.padding(end = 5.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(stringResource(R.string.events_count, item.eventCount))
-                        if (item.running) {
-                            val transition = rememberInfiniteTransition(label = "running_icon")
-                            val alphaCycle by transition.animateFloat(
-                                initialValue = .3F,
-                                targetValue = 1F,
-                                animationSpec = InfiniteRepeatableSpec(
-                                    animation = tween(durationMillis = 1000),
-                                    repeatMode = RepeatMode.Reverse
-                                ),
-                                label = "alpha_cycle"
-                            )
-                            Icon(
-                                modifier = Modifier.graphicsLayer { alpha = alphaCycle },
-                                painter = painterResource(R.drawable.running),
-                                contentDescription = "running",
-                            )
-                        }
-                    }
-                    Spacer(modifier = Modifier.width(10.dp))
-                }
+                SessionListItem(session, outerNavHostController, onAction)
             }
         }
     }
