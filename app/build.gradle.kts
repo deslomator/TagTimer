5c5dcd38b6662d42400cb2ee9ef4b8f1f1350054
daf <mail@mail.com>
2025-03-15 10:49:47 +0100

    Updated some libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ kotlin {
 dependencies {
 
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.1.10"))
-    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.03.00"))
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
 
@@ -80,8 +80,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 
-    implementation("androidx.navigation:navigation-compose:2.8.8")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.8.8")
+    implementation("androidx.navigation:navigation-compose:2.8.9")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.8.9")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     // room
@@ -94,7 +94,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.02.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4-android:1.7.8")
 
     debugImplementation("androidx.compose.ui:ui-tooling-android:1.7.8")
